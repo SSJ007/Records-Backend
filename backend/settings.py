8002fe21f3185568e8294eaa6301bcfd265a8598
Sanskar <sanskarjha007@gmail.com>
2021-01-08 22:34:23 +0530

    Insatlled psycopg2

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '($4l12fpoxw#48ty!w+kgf-v+yv5tx%=iw678+wq^o12m&#sk^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'sanskar-almabetter-challenge.herokuapp.com']
 
